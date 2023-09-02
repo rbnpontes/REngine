ac834a46bebfcd9d463b8a9dd938e675b35cecf5
Ruben Gomes <rbnpontes@gmail.com>
2023-09-02 18:50:43 -0300

    feat: setup project

---
+++ REngine.RHI/IGraphicsDriver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.RHI
+{
+	public enum GraphicsBackend
+	{
+		Unknow,
+		D3D11,
+		D3D12,
+		Metal,
+		Vulkan,
+		OpenGL,
+		Software
+	}
+	public interface IGraphicsDriver
+	{
+		public GraphicsBackend Backend { get; }
+		public string DriverName { get; }
+	}
+}
