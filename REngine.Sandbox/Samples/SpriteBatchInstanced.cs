ea43fae908c586046399f5dd15a7fa42f35a50ae
Ruben Gomes <rbnpontes@gmail.com>
2023-09-25 07:34:27 -0300

    feat: add sample window fix: concurrency issues at SpriteTextureManager feat: add clear textures on sprite batch

---
+++ REngine.Sandbox/Samples/SpriteBatchInstanced.cs
@@ -0,0 +1,90 @@
+﻿using REngine.Assets;
+using REngine.Core;
+using REngine.Core.DependencyInjection;
+using REngine.RPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.Sandbox.Samples
+{
+#if RENGINE_SPRITEBATCH
+	[Sample("SpriteBatch - Instanced")]
+	internal class SpriteBatchInstanced : ISample
+	{
+		public IWindow? Window { get; set; }
+
+		private ISpriteBatch? pSpriteBatch;
+		private IRenderFeature? pSpriteFeature;
+		private IRenderer? pRenderer;
+		private IEngine? pEngine;
+
+		private Size pInstancedSpriteSize;
+		private SpriteInstancedBatchInfo[] pSpriteInstances;
+
+		public SpriteBatchInstanced()
+		{
+			pInstancedSpriteSize = new Size(20, 20);
+			pSpriteInstances = new SpriteInstancedBatchInfo[pInstancedSpriteSize.Width * pInstancedSpriteSize.Height];
+		}
+
+		public void Dispose()
+		{
+			pSpriteInstances = new SpriteInstancedBatchInfo[0];
+			pRenderer?.RemoveFeature(pSpriteFeature);
+			pSpriteBatch?.ClearTextures();
+		}
+
+		public void Load(IServiceProvider provider)
+		{
+			pSpriteBatch = provider.Get<ISpriteBatch>();
+
+			// Load Sprite
+			ImageAsset sprite = new ImageAsset("doge.png");
+			using (FileStream stream = new FileStream(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Assets/Textures/doge.jpg"), FileMode.Open))
+				sprite.Load(stream).Wait();
+
+			// Set Sprite on Spritebatch
+			pSpriteBatch.SetTexture(0, sprite.Image);
+			pSpriteFeature = pSpriteBatch.Feature;
+
+			pRenderer = provider.Get<IRenderer>().AddFeature(pSpriteFeature);
+			pEngine = provider.Get<IEngine>();
+		}
+
+		public void Update(IServiceProvider provider)
+		{
+			float elapsedTime = (float)(pEngine?.ElapsedTime ?? 0.0) / 1000.0f;
+			Size wndSize = Window?.Size ?? new Size();
+
+			pSpriteBatch?.Draw(0, GetSpriteInstances(elapsedTime, wndSize));
+		}
+
+		private IEnumerable<SpriteInstancedBatchInfo> GetSpriteInstances(float elapsed, Size wndSize)
+		{
+			Vector2 size = new Vector2((5 + ((1 + (float)Math.Sin(elapsed)) * 0.5f) * 100));
+			Vector2 anchor = new Vector2(0.5f, 0.5f);
+			return pSpriteInstances
+					.AsParallel()
+					.Select((sprite, i) =>
+					{
+						int x = i % pInstancedSpriteSize.Width;
+						int y = i / pInstancedSpriteSize.Height;
+						return new SpriteInstancedBatchInfo
+						{
+							Size = size,
+							Angle = (x - y) + elapsed,
+							Anchor = anchor,
+							Position = new Vector2(
+								(x / 20.0f) * wndSize.Width,
+								(y / 20.0f) * wndSize.Height
+							)
+						};
+					});
+		}
+	}
+#endif
+}
---
+++ REngine.Sandbox/Samples/SpriteBatchSample.cs
@@ -0,0 +1,88 @@
+﻿using REngine.Assets;
+using REngine.Core;
+using REngine.Core.DependencyInjection;
+using REngine.Core.Mathematics;
+using REngine.RPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.Sandbox.Samples
+{
+#if RENGINE_SPRITEBATCH
+	[Sample("SpriteBatch")]
+	internal class SpriteBatchSample : ISample
+	{
+		public IWindow? Window { get; set; }
+		private ISpriteBatch? pSpriteBatch;
+		private IRenderFeature? pSpriteFeature;
+		private IRenderer? pRenderer;
+		private IEngine? pEngine;
+
+		public void Dispose()
+		{
+			pRenderer?.RemoveFeature(pSpriteFeature);
+			pSpriteBatch?.ClearTextures();
+		}
+
+		public void Load(IServiceProvider provider)
+		{
+			pSpriteBatch = provider.Get<ISpriteBatch>();
+
+			// Load Sprite
+			ImageAsset sprite = new ImageAsset("doge.png");
+			using (FileStream stream = new FileStream(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Assets/Textures/doge.jpg"), FileMode.Open))
+				sprite.Load(stream).Wait();
+
+			// Set Sprite on Spritebatch
+			pSpriteBatch.SetTexture(0, sprite.Image);
+			pSpriteFeature = pSpriteBatch.Feature;
+
+			pRenderer = provider.Get<IRenderer>().AddFeature(pSpriteFeature);
+			pEngine = provider.Get<IEngine>();
+		}
+
+		public void Update(IServiceProvider provider)
+		{
+			float elapsedTime = (float)(pEngine?.ElapsedTime ?? 0.0) / 1000.0f;
+			Size wndSize = Window?.Size ?? new Size();
+			Vector2 halfSize = new Vector2(wndSize.Width / 2.0f, wndSize.Height / 2.0f);
+
+			float stagger = AnalogicTime(elapsedTime + 0.5f, 2.5f, 3);
+			float sineT = stagger * (float)Math.Sin(elapsedTime);
+			float cosT = stagger * (float)Math.Cos(elapsedTime);
+
+			// Draw Flickering Doge
+			pSpriteBatch?.Draw(new SpriteBatchInfo
+			{
+				/*Batch texture slot*/
+				TextureSlot = 0,
+				Size = new Vector2(300) * AnalogicTime(elapsedTime, 1f, 2),
+				Angle = elapsedTime,
+				Anchor = new Vector2(0.5f, 0.5f),
+				Position = halfSize + (new Vector2(cosT, sineT) * 150)
+			});
+			// Draw Colored Doge
+			pSpriteBatch?.Draw(new SpriteBatchInfo
+			{
+				TextureSlot = 0,
+				Angle = elapsedTime,
+				Anchor = new Vector2(0.5f, 0.5f),
+				Position = halfSize,
+				Size = new Vector2(150),
+				Color = ColorUtils.FromHSL(elapsedTime, 1, 1)
+			});
+		}
+
+		private float AnalogicTime(float t, float freq, float amplitude)
+		{
+			t = (float)(Math.Sin(t * freq) * amplitude);
+			t = Math.Clamp(t, -(float)Math.Round(freq), (float)Math.Round(freq));
+			return (float)Math.Floor(t);
+		}
+	}
+#endif
+}
