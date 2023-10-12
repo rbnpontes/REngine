3c8cba4d3deff9c2932c41ac2c5d28d2f4cdb5a8
Ruben Gomes <rbnpontes@gmail.com>
2023-10-12 08:43:19 -0300

    chore: update imgui update rate

--- REngine.RPI/RenderSettings.cs
+++ REngine.RPI/RenderSettings.cs
@@ -50,7 +50,7 @@ namespace REngine.RPI
 		public uint SpriteBatchTexturesBuildTimeMs { get; set; } = 2;
 #endif
 #if RENGINE_IMGUI
-		public float ImGuiUpdateRate { get; set; } = 16.0f; // Update ImGui at 60FPS
+		public float ImGuiUpdateRate { get; set; } = 33.0f; // Update ImGui at 30FPS
 #endif
 		public void Merge(RenderSettings settings)
 		{
