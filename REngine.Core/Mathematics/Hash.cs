bb15c5de96dc85977a49e8528878fd71058b50ea
Ruben Gomes <rbnpontes@gmail.com>
2023-11-09 07:17:18 -0300

    feat: add hash

---
+++ REngine.Core/Mathematics/Hash.cs
@@ -0,0 +1,17 @@
+﻿using Standart.Hash.xxHash;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.Core.Mathematics
+{
+	public static class Hash
+	{
+		public static ulong Digest(string input)
+		{
+			return xxHash64.ComputeHash(input);
+		}
+	}
+}
