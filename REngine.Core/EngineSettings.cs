55a45c10023bd9d941928ad08178c42b6689fc69
Ruben Gomes <rbnpontes@gmail.com>
2023-11-11 10:35:20 -0300

    feat: add shader manager

--- REngine.Core/EngineSettings.cs
+++ REngine.Core/EngineSettings.cs
@@ -15,6 +15,9 @@ namespace REngine.Core
 		public static readonly string RenderSettingsPath = Path.Combine(AppDataPath, "render.rcfgs");
 		public static readonly string GraphicsSettingsPath = Path.Combine(AppDataPath, "graphics.rcfgs");
 		public static readonly string DriverSettingsPath = Path.Combine(AppDataPath, "driver.rcfgs");
+		public static readonly string ShaderCachePath = Path.Combine(AppDataPath, "shadercache");
+		public static readonly string PipelineCachePath = Path.Combine(AppDataPath, "pipelinecache.bin");
+		public static readonly string PipelineItemsCachePath = Path.Combine(AppDataPath, "pipelinecache.rcfgs");
 
 		public EngineSettings() { }
 
