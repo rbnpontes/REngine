f6dd8cf30c088bdc2584f33f96b40d7577623248
Ruben Gomes <rbnpontes@gmail.com>
2023-11-25 23:44:14 -0300

    refactor: change pong main menu state to use pong menu buttons component

--- REngine.Sandbox/States/PongMainMenuState.cs
+++ REngine.Sandbox/States/PongMainMenuState.cs
@@ -14,24 +14,13 @@ using REngine.Sandbox.Components;
 
 namespace REngine.Sandbox.States
 {
-	internal class PongMainMenuState : IGameState
+	internal class PongMainMenuState(
+		EntityManager entityMgr,
+		IWindow mainWindow,
+		GameStateManager gameStateManager)
+		: IGameState
 	{
-		private readonly EntityManager pEntityManager;
-		private readonly IWindow pMainWindow;
-		private readonly GameStateManager pGameStateManager;
-
-		private Transform2D? pMenuTransform;
 		public string Name => nameof(PongMainMenuState);
-
-		public PongMainMenuState(
-			EntityManager entityMgr,
-			IWindow mainWindow,
-			GameStateManager gameStateManager)
-		{
-			pEntityManager = entityMgr;
-			pMainWindow = mainWindow;
-			pGameStateManager = gameStateManager;
-		}
 
 		public void OnStart()
 		{
@@ -40,64 +29,34 @@ namespace REngine.Sandbox.States
 				PongVariables.BackgroundAudio.Volume = PongVariables.AudioVolume;
 				PongVariables.BackgroundAudio.Loop = true;
 				PongVariables.BackgroundAudio.Pitch = 1;
+				PongVariables.BackgroundAudio.Offset = TimeSpan.Zero;
 				PongVariables.BackgroundAudio.Play();
 			}
 
-			var menu = pEntityManager.CreateEntity("menu");
-			var rootTransform = menu.CreateComponent<Transform2D>();
-
-			pMenuTransform = rootTransform;
-			UpdateMenuPosition();
-
-			var menuButton = pEntityManager.CreateEntity("menu:play");
-			var transform = menuButton.CreateComponent<Transform2D>();
-			var button = menuButton.CreateComponent<PongMenuButton>();
-
-			button.HoverAudio = PongVariables.MenuItemAudio;
-			button.TextureSlot = PongVariables.MenuPlayButtonSlot;
-			button.ClickAction = ExecutePlay;
-
-			pMenuTransform.AddChild(transform);
-
-			menuButton = pEntityManager.CreateEntity("menu:exit");
-			transform = menuButton.CreateComponent<Transform2D>();
-			button = menuButton.CreateComponent<PongMenuButton>();
-
-			button.HoverAudio = PongVariables.MenuItemAudio;
-			button.TextureSlot = PongVariables.MenuExitButtonSlot;
-			button.ClickAction = ExecuteStop;
-
-			transform.Position = new Vector2(0, PongVariables.MenuTextureSize.Y + PongVariables.MenuButtonMargin);
-			pMenuTransform.AddChild(transform);
+			var menu = entityMgr.CreateEntity("menu");
+			var mainMenu = menu.CreateComponent<PongMainMenu>();
+			mainMenu.Visible = true;
+			mainMenu.PlayAction = ExecutePlay;
+			mainMenu.ExitAction = ExecuteStop;
 		}
 
 		public void OnUpdate()
 		{
-			UpdateMenuPosition();
 		}
 
 		public void OnExit()
 		{
-			pEntityManager.DestroyAll();
+			entityMgr.DestroyAll();
 		}
 
 		private void ExecutePlay()
 		{
-			pGameStateManager.SetState(PongStates.PongGamePlayState);
+			gameStateManager.SetState(PongStates.PongGamePlayState);
 		}
 
 		private void ExecuteStop()
 		{
-			pMainWindow.Close();
-		}
-
-		private void UpdateMenuPosition()
-		{
-			if (pMenuTransform is null)
-				return;
-
-			var contentSize = PongVariables.MenuTextureSize with { Y = PongVariables.MenuButtonMargin + PongVariables.MenuTextureSize.Y * 2 };
-			pMenuTransform.Position = (pMainWindow.Size.ToVector2() * 0.5f) - contentSize * 0.5f;
+			mainWindow.Close();
 		}
 	}
 }
