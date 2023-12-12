0692abe9f89364f32db7c7965054faaf9ad5fe3a
Ruben Gomes <rbnpontes@gmail.com>
2023-12-12 08:12:40 -0300

    fix: dll importer on android

--- REngine.Android.Sandbox/SandboxApp.cs
+++ REngine.Android.Sandbox/SandboxApp.cs
@@ -1,27 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Android.Media;
-using Android.Util;
-using Android.Views;
-using REngine.Android.Windows;
-using REngine.Core;
-using REngine.Core.Android;
-using REngine.Core.DependencyInjection;
-using REngine.Core.IO;
+﻿using REngine.Core.DependencyInjection;
 using REngine.Core.Resources;
-using REngine.RHI;
-using REngine.RHI.DiligentDriver;
-using REngine.RPI;
-using IWindowManager = REngine.Core.IWindowManager;
-using WindowManager = REngine.Android.Windows.WindowManager;
+using REngine.Sandbox.Samples;
 
 namespace REngine.Android.Sandbox
 {
-	public class SandboxApp() : App
+	public class SandboxApp : SampleApp
 	{
 		public override void OnSetup(IServiceRegistry registry)
 		{
@@ -31,6 +14,8 @@ namespace REngine.Android.Sandbox
 			registry
 				.Add(()=> assetManager)
 				.Add<IAssetManager, HttpAssetManager>();
+			
+			base.OnSetup(registry);
 		}
 
 		protected override void OnGui()
