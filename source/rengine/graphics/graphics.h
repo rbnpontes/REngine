51d3745d2229b7a3c42221448e23b1f933637dcf
Ruben Gomes <rbnpontes@gmail.com>
2025-04-03 11:57:46 -0300

    feat: add graphics manager includes

--- source/rengine/graphics/graphics.h
+++ source/rengine/graphics/graphics.h
@@ -2,3 +2,5 @@
 #include <rengine/graphics/buffers.h>
 #include <rengine/graphics/renderer.h>
 #include <rengine/graphics/models.h>
+#include <rengine/graphics/shader_manager.h>
+#include <rengine/graphics/pipeline_state_manager.h>
