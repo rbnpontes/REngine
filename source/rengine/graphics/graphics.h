2f635368b15199cf66b761362c3eb385b3b6fc5c
Ruben Gomes <rbnpontes@gmail.com>
2025-04-05 19:16:51 -0300

    feat: add forgotten buffer manager header

--- source/rengine/graphics/graphics.h
+++ source/rengine/graphics/graphics.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <rengine/graphics/buffers.h>
 #include <rengine/graphics/renderer.h>
 #include <rengine/graphics/models.h>
 #include <rengine/graphics/shader_manager.h>
 #include <rengine/graphics/pipeline_state_manager.h>
+#include <rengine/graphics/buffer_manager.h>
