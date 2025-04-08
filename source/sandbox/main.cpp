03be8c913efdaa6dc0f4624de8c21d384c4d3f6d
Ruben Gomes <rbnpontes@gmail.com>
2025-04-07 23:38:25 -0300

    chore: update sandbox

--- source/sandbox/main.cpp
+++ source/sandbox/main.cpp
@@ -9,12 +9,20 @@ void game_loop() {
         return;
     }
 
+    /*rengine::graphics::renderer_begin_draw();
+        rengine::graphics::renderer_add_triangle({
+            { rengine::math::vec3(-0.5f, -0.5f, 0.), rengine::math::byte_color::red },
+            { rengine::math::vec3(+0.0f, +0.5f, 0.), rengine::math::byte_color::green },
+            { rengine::math::vec3(+0.5f, -0.5f, 0.), rengine::math::byte_color::blue },
+        });
+    rengine::graphics::renderer_end_draw();*/
 }
 
-int main(unsigned int argc, char** argv) {
+int main(unsigned int argc, char** argv) {   
     rengine::init();
     const auto wnd = rengine::core::window_create("REngine", 500, 400);
     g_window_id = wnd;
+    rengine::use_window(wnd);
     rengine::run(game_loop);
     rengine::destroy();
     return 0;
